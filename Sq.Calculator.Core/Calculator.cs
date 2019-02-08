9a0cfc37c2e2577af11b9e18dcbc1a8d9324dedf
Andreas Palmblad <p.blad@hotmail.com>
2019-02-08 11:03:25 +0100

    Add project files.

---
+++ Sq.Calculator.Core/Calculator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sq.Calculator.Core
+{
+    public class Calculator
+    {
+        public List<OperationHistoryItem> OperationHistory { get; set; } = new List<OperationHistoryItem>();
+        public decimal State { get; set; }
+
+        public decimal Add(decimal x)
+        {
+            State += x;
+            OperationHistory.Add(new OperationHistoryItem(x, Operator.Add));
+            return State;
+        }
+    }
+}
---
+++ Sq.Calculator.Core/OperationHistoryItem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sq.Calculator.Core
+{
+    public enum Operator
+    {
+        Add,
+        Subtract,
+        Multiply,
+        Divide
+    }
+    public class OperationHistoryItem
+    {
+        public decimal Value { get; set; }
+        public Operator Operator { get; set; }
+
+        public OperationHistoryItem(decimal value, Operator op)
+        {
+            Value = value;
+            Operator = op;
+        }
+    }
+}
